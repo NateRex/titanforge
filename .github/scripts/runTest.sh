7f30ef1d953deafa426ae5b7ef2cc94d09800f92
naterex <nathanieljrex@gmail.com>
2025-02-22 22:15:20 -0500

    Script typo

--- .github/scripts/runTest.sh
+++ .github/scripts/runTest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 chmod +x $1
-valgrind --leak-check=full -$1 &> test_output.txt
+valgrind --leak-check=full $1 &> test_output.txt
 export code=$?
 cat test_output.txt
 if [ $code -ne 0 ]; then
