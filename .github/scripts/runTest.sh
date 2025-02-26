35977b84681fc0415abd746dbd6502a81d9abceb
naterex <nathanieljrex@gmail.com>
2025-02-25 20:35:40 -0500

    Fix new grep command

--- .github/scripts/runTest.sh
+++ .github/scripts/runTest.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 chmod +x $1
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --suppressions=/usr/local/libexec/valgrind/default.supp $1 &> test_output.txt
+valgrind --leak-check=full --suppressions=/usr/local/libexec/valgrind/default.supp $1 &> test_output.txt
 export code=$?
 cat test_output.txt
 if [ $code -ne 0 ]; then
     exit $code
 fi
-if ! grep -q "no leaks are possible|definitely lost: 0 bytes in 0 blocks" test_output.txt; then
+if ! grep -q -e "no leaks are possible" -e "definitely lost: 0 bytes in 0 blocks" test_output.txt; then
     exit 1
 fi
