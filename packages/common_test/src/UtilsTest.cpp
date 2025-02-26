bb9fed45066119997a70e6b7f7fbd45a6f493626
naterex <nathanieljrex@gmail.com>
2025-02-25 20:57:55 -0500

    suppress uninitialized variable warnings

--- packages/common_test/src/UtilsTest.cpp
+++ packages/common_test/src/UtilsTest.cpp
@@ -53,11 +53,9 @@ BOOST_AUTO_TEST_CASE(Utils_getOrDefault)
 {
     double one = 1.;
     double* ptr = nullptr;
-    double value = getOrDefault(ptr, one);
-    BOOST_TEST(value == one);
+    BOOST_TEST((getOrDefault(ptr, one) == one));
 
     double two = 2.;
     ptr = &two;
-    value = getOrDefault(ptr, one);
-    BOOST_TEST(value == two);
+    BOOST_TEST((getOrDefault(ptr, one) == two));
 }
