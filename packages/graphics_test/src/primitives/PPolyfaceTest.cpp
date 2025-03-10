8f16cc82ce45e7e9b728b34a92e97533531491ef
naterex <nathanieljrex@gmail.com>
2025-03-09 20:32:03 -0400

    New primitive tests

--- packages/graphics_test/src/primitives/PPolyfaceTest.cpp
+++ packages/graphics_test/src/primitives/PPolyfaceTest.cpp
@@ -2,42 +2,82 @@
 #include <graphics/Engine.h>
 #include <graphics/window/Window.h>
 #include <graphics/primitives/PPolyface.h>
+#include <graphics/primitives/PrimitiveAttributes.h>
 #include <geometry/Vector3.h>
+
+/**
+ * Helper method for getting sample data that can be used to construct a PPolyface
+ * @param vertices Vector in which to store vertex positions
+ * @param indices Vector in which to store vertex indices
+ * @param colors (Optional) Vector in which to store colors
+ */
+void getSampleData(std::vector<Vector3>& vertices, std::vector<int>& indices,
+		std::vector<Color>& colors = std::vector<Color>())
+{
+	vertices.push_back(Vector3(-1, -1, 0));
+	vertices.push_back(Vector3(-1, 1, 0));
+	vertices.push_back(Vector3(1, 1, 0));
+	vertices.push_back(Vector3(1, -1, 0));
+
+	colors.push_back(Color::fromFloats(1, 0, 0, 1));
+	colors.push_back(Color::fromFloats(0, 1, 0, 1));
+	colors.push_back(Color::fromFloats(0, 0, 1, 1));
+	colors.push_back(Color::fromFloats(1, 1, 0, 1));
+
+	indices.push_back(0);
+	indices.push_back(1);
+	indices.push_back(2);
+	indices.push_back(-1);
+	indices.push_back(0);
+	indices.push_back(2);
+	indices.push_back(3);
+	indices.push_back(-1);
+}
 
 /**
  * Tests the basic constructors of the class
  */
 BOOST_AUTO_TEST_CASE(PPolyface_basics)
 {
-	// Test using raw arrays
-	Vector3 pts[4] = {
-		Vector3(-1, -1, 0),
-		Vector3(1, -1, 0),
-		Vector3(0, 1, 0),
-		Vector3(0, 0, 1)
-	};
-	int verts[16] = {
-		0, 1, 2, -1,
-		0, 1, 3, -1,
-		1, 2, 3, -1,
-		2, 0, 3, -1
-	};
+	std::vector<Vector3> vertices;
+	std::vector<int> indices;
+	getSampleData(vertices, indices);
 
-	PPolyface p(pts, 4, verts, 16);
-	BOOST_TEST(p.getNumVertices() == 12);
-	BOOST_TEST(p.getNumFacets() == 4);
+	// Test using raw pointers
+	PPolyface p(vertices.data(), vertices.size(), indices.data(), indices.size());
+	BOOST_TEST(p.getNumVertices() == 6);
+	BOOST_TEST(p.getNumFacets() == 2);
 
 	// Test using vectors
-	std::vector<Vector3> ptsVec(pts, pts + 4);
-	std::vector<int> vertsVec(verts, verts + 16);
-	p = PPolyface(ptsVec, vertsVec);
-	BOOST_TEST(p.getNumVertices() == 12);
-	BOOST_TEST(p.getNumFacets() == 4);
+	p = PPolyface(vertices, indices);
+	BOOST_TEST(p.getNumVertices() == 6);
+	BOOST_TEST(p.getNumFacets() == 2);
 
 	// Test using existing polyface
 	PPolyface p2(p);
-	BOOST_TEST(p2.getNumVertices() == 12);
-	BOOST_TEST(p2.getNumFacets() == 4);
+	BOOST_TEST(p2.getNumVertices() == 6);
+	BOOST_TEST(p2.getNumFacets() == 2);
+}
+
+/**
+ * Tests the ability to obtain the attributes of a polyface primitive
+ */
+BOOST_AUTO_TEST_CASE(PPolyface_attributes)
+{
+	std::vector<Vector3> vertices;
+	std::vector<Color> colors;
+	std::vector<int> indices;
+	getSampleData(vertices, indices, colors);
+
+	// Without color
+	PPolyface poly(vertices, indices);
+	PrimitiveAttributes attributes = poly.getAttributes();
+	BOOST_TEST(!attributes.hasColor);
+
+	// With color
+	poly = PPolyface(vertices, indices, colors);
+	attributes = poly.getAttributes();
+	BOOST_TEST(attributes.hasColor);
 }
 
 /**
@@ -45,23 +85,24 @@ BOOST_AUTO_TEST_CASE(PPolyface_basics)
  */
 BOOST_AUTO_TEST_CASE(PPolyface_buffer)
 {
-	Vector3 pts[4] = {
-		Vector3(-1, -1, 0),
-		Vector3(1, -1, 0),
-		Vector3(0, 1, 0),
-		Vector3(0, 0, 1)
-	};
-	int verts[16] = {
-		0, 1, 2, -1,
-		0, 1, 3, -1,
-		1, 2, 3, -1,
-		2, 0, 3, -1
-	};
-	PPolyface poly(pts, 4, verts, 16);
+	std::vector<Vector3> vertices;
+	std::vector<Color> colors;
+	std::vector<int> indices;
+	getSampleData(vertices, indices, colors);
 
-	std::vector<float> vertexBuffer;
-	std::vector<int> indexBuffer;
-	poly.buffer(vertexBuffer, indexBuffer);
-	BOOST_TEST(vertexBuffer.size() == 12);
-	BOOST_TEST(indexBuffer.size() == 12);
+	// Without color
+	PPolyface poly(vertices, indices);
+	std::vector<float> vBuffer;
+	std::vector<int> iBuffer;
+	poly.buffer(vBuffer, iBuffer);
+	BOOST_TEST(vBuffer.size() == 12);
+	BOOST_TEST(iBuffer.size() == 6);
+
+	// With color
+	poly = PPolyface(vertices, indices, colors);
+	vBuffer.clear();
+	iBuffer.clear();
+	poly.buffer(vBuffer, iBuffer);
+	BOOST_TEST(vBuffer.size() == 28);
+	BOOST_TEST(iBuffer.size() == 6);
 }
