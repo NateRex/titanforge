3b39dc5d88c2b37d59db66d4f0b073f7d16e3ba9
naterex <nathanieljrex@gmail.com>
2025-03-12 20:10:17 -0400

    Try making linked libs public

--- packages/graphics_test/src/textures/TextureTest.cpp
+++ packages/graphics_test/src/textures/TextureTest.cpp
@@ -6,20 +6,20 @@
 /**
  * Tests construction and destruction of a texture using the texture manager
  */
-//BOOST_AUTO_TEST_CASE(Texture_basics)
-//{
-//	Texture texture = TextureManager::create("texture", "assets/container.jpg");
-//	BOOST_TEST(texture.name == "texture");
-//	BOOST_TEST(texture.imagePath == "assets/container.jpg");
-//
-//	BOOST_REQUIRE_NO_THROW(TextureManager::destroy("texture"));
-//}
-//
-///**
-// * Tests that an instantiation exception is thrown trying to create a texture using an
-// * image that does not exist
-// */
-//BOOST_AUTO_TEST_CASE(Texture_nonExistentImage)
-//{
-//	BOOST_REQUIRE_THROW(TextureManager::create("texture", "does-not-exist"), InstantiationException);
-//}
+BOOST_AUTO_TEST_CASE(Texture_basics)
+{
+	Texture texture = TextureManager::create("texture", "assets/container.jpg");
+	BOOST_TEST(texture.name == "texture");
+	BOOST_TEST(texture.imagePath == "assets/container.jpg");
+
+	BOOST_REQUIRE_NO_THROW(TextureManager::destroy("texture"));
+}
+
+/**
+ * Tests that an instantiation exception is thrown trying to create a texture using an
+ * image that does not exist
+ */
+BOOST_AUTO_TEST_CASE(Texture_nonExistentImage)
+{
+	BOOST_REQUIRE_THROW(TextureManager::create("texture", "does-not-exist"), InstantiationException);
+}
