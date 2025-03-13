76dcceabe099dd7af0bcd56f633d94b6f6bbe25d
naterex <nathanieljrex@gmail.com>
2025-03-12 20:43:57 -0400

    Try storing valgrind logs as build artifacts

--- .github/scripts/runTest.sh
+++ .github/scripts/runTest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 chmod +x $1
-valgrind --leak-check=full --undef-value-errors=no --suppressions=/usr/local/libexec/valgrind/default.supp $1 &> test_output.txt
+valgrind --leak-check=full --undef-value-errors=no --log-file=valgrind.log --suppressions=/usr/local/libexec/valgrind/default.supp $1 &> test_output.txt
 export code=$?
 cat test_output.txt
 if [ $code -ne 0 ]; then
