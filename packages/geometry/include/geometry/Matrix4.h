c2d19a8ef7df53f8c22a98cc28652aeeddc6fd4c
naterex <nathanieljrex@gmail.com>
2025-03-20 18:01:23 -0400

    Matrix4 header

--- packages/geometry/include/geometry/Matrix4.h
+++ packages/geometry/include/geometry/Matrix4.h
@@ -1,93 +1,81 @@
 #pragma once
 
 class Vector3;
+class Matrix3;
 
 /**
- * A 3x3 matrix, whose values are indexable as a row-major order array
+ * A 4x4 matrix, whose values are indexable as a row-major order array.
  * @author Nathaniel Rex
  */
-class Matrix3
-{
+class Matrix4 {
 public:
 
 	/**
 	 * Identity matrix
 	 */
-	static const Matrix3 IDENTITY;
+	static const Matrix4 IDENTITY;
 
 	/**
-	 * Constructs the identity matrix
+	 * Construct the identity matrix
 	 */
-	Matrix3();
+	Matrix4();
 
 	/**
-	 * Constructs a matrix from values defined in row-major order.
+	 * Constructs a matrix from values defined in row-major order
 	 * @param m00 Row 0, column 0 value
 	 * @param m01 Row 0, column 1 value
 	 * @param m02 Row 0, column 2 value
+	 * @param m03 Row 0, column 3 value
 	 * @param m10 Row 1, column 0 value
 	 * @param m11 Row 1, column 1 value
 	 * @param m12 Row 1, column 2 value
+	 * @param m13 Row 1, column 3 value
 	 * @param m20 Row 2, column 0 value
 	 * @param m21 Row 2, column 1 value
 	 * @param m22 Row 2, column 2 value
+	 * @param m23 Row 2, column 3 value
+	 * @param m30 Row 3, column 0 value
+	 * @param m31 Row 3, column 1 value
+	 * @param m32 Row 3, column 2 value
+	 * @param m33 Row 3, column 3 value
 	 */
-	Matrix3(float m00, float m01, float m02, float m10, float m11, float m12, float m20, float m21, float m22);
+	Matrix4(float m00, float m01, float m02, float m03, float m10, float m11, float m12, float m13,
+		float m20, float m21, float m22, float m23, float m30, float m31, float m32, float m33);
 
 	/**
-	 * Constructs a matrix from another 3x3 matrix
-	 * @param other Matrix to copy
+	 * Constructs a matrix from another 4x4 matrix
+	 * @param other Matrix to copy from
 	 */
-	Matrix3(const Matrix3& other);
+	Matrix4(const Matrix4& other);
 
 	/**
 	 * Destructor
 	 */
-	~Matrix3();
+	~Matrix4();
 
 	/**
-	 * Constructs a matrix from three row vectors
-	 * @param r0 Row 0 vector
-	 * @param r1 Row 1 vector
-	 * @param r2 Row 2 vector
-	 * @param result (Optional) Matrix to store the result in
-	 * @return The matrix containing the given row vectors
-	 */
-	static Matrix3 fromRows(const Vector3& r0, const Vector3& r1, const Vector3& r2, Matrix3* result = nullptr);
-
-	/**
-	 * Constructs a matrix from three column vectors
-	 * @param c0 Column 0 vector
-	 * @param c1 Column 1 vector
-	 * @param c2 Column 2 vector
-	 * @param result (Optional) Matrix to store the result in
-	 * @return The matrix containing the given column vectors
-	 */
-	static Matrix3 fromColumns(const Vector3& c0, const Vector3& c1, const Vector3& c2, Matrix3* result = nullptr);
-
-	/**
-	 * Constructs a matrix representing a rotation about the X-axis
-	 * @param radians Angle of rotation, in radians
+	 * Constructs a transformation matrix representing a rotation
+	 * @param rot 3x3 rotation matrix
 	 * @param (Optional) Matrix in which to store the results
-	 * @return The rotation matrix
+	 * @return The transformation matrix
 	 */
-	static Matrix3 fromXRotation(float radians, Matrix3* result = nullptr);
+	static Matrix4 fromRotation(const Matrix3& rot, Matrix4* result = nullptr);
 
 	/**
-	 * Constructs a matrix representing a rotation about the Y-axis
-	 * @param radians Angle of rotation, in radians
+	 * Constructs a transformation matrix representing a translation
+	 * @param translation Vector position representing the translation
 	 * @param (Optional) Matrix in which to store the results
-	 * @return The rotation matrix
+	 * @return The transformation matrix
 	 */
-	static Matrix3 fromYRotation(float radians, Matrix3* result = nullptr);
+	static Matrix4 fromTranslation(const Vector3& translation, Matrix4* result = nullptr);
 
 	/**
-	 * Constructs a matrix representing a rotation about the Z-axis
-	 * @param radians Angle of rotation, in radians
+	 * Constructs a transformation matrix representing a scaling
+	 * @param scalar Value representing the scale magnitude
 	 * @param (Optional) Matrix in which to store the results
-	 * @return The rotation matrix
+	 * @return The transformation matrix
 	 */
-	static Matrix3 fromZRotation(float radians, Matrix3* result = nullptr);
+	static Matrix4 fromScale(float scale, Matrix4* result = nullptr);
 
 	/**
 	 * @return True if this matrix is the identity matrix. Returns false otherwise.
@@ -100,14 +88,14 @@ public:
 	 * @param (Optional) Tolerance value. Defaults to 0.
 	 * @return True if the two matrices are equal (within tolerance). False otherwise.
 	 */
-	bool equalTo(const Matrix3& other, float tol = 0.) const;
+	bool equalTo(const Matrix4& other, float tol = 0.) const;
 
 	/**
 	 * Computes the transpose of this matrix
 	 * @param result (Optional) Matrix in which to store the result
 	 * @return The transpose matrix
 	 */
-	Matrix3 transpose(Matrix3* result = nullptr) const;
+	Matrix4 transpose(Matrix4* result = nullptr) const;
 
 	/**
 	 * Computes the inverse of this matrix
@@ -115,15 +103,25 @@ public:
 	 * the inverse does not exist.
 	 * @return A boolean value indicating whether or not the inverse exists.
 	 */
-	bool inverse(Matrix3* result);
+	bool inverse(Matrix4* result);
 
 	/**
-	 * Compute the product of this matrix and a column vector
-	 * @param v The column vector
-	 * @param result (Optional) Vector in which to store the result
-	 * @return The product of this matrix and the column vector
+	 * Transforms the given 3-dimensional position vector using this matrix. This will result in a new
+	 * position after having applied scaling, rotation, and translation.
+	 * @param p Position vector to transform
+	 * @param result (Optional) Vector in which to store the result.
+	 * @return The resulting position vector
 	 */
-	Vector3 multiply(const Vector3& v, Vector3* result = nullptr) const;
+	Vector3 transformPosition(const Vector3& p, Vector3* result = nullptr) const;
+
+	/**
+	 * Transforms the given 3-dimensional direction vector using this matrix. This will result in a new
+	 * direction after having applied scaling and rotation.
+	 * @param v Direction vector to transform
+	 * @param result (Optional) Vector in which to store the result
+	 * @return The resulting direction vector
+	 */
+	Vector3 transformDirection(const Vector3& v, Vector3* result = nullptr) const;
 
 	/**
 	 * Compute the product of this matrix and another matrix
@@ -131,7 +129,7 @@ public:
 	 * @param result (Optional) Matrix in which to store the result
 	 * @return The product of this matrix and the other matrix
 	 */
-	Matrix3 multiply(const Matrix3& m, Matrix3* result = nullptr) const;
+	Matrix4 multiply(const Matrix4& m, Matrix4* result = nullptr) const;
 
 	/**
 	 * Access a value of this matrix at the given index. Values of the matrix are stored and accessed in
@@ -146,28 +144,28 @@ public:
 	 * @param other Matrix to assign to
 	 * @return The matrix instance after reassignment
 	 */
-	Matrix3& operator=(const Matrix3& other);
+	Matrix4& operator=(const Matrix4& other);
 
 	/**
 	 * Operator that compares this matrix to another matrix to determine if they are equal.
-     * @param other The matrix to compare to
-     * @return True if the two matrices are equal in value. Returns false otherwise.
+	 * @param other The matrix to compare to
+	 * @return True if the two matrices are equal in value. Returns false otherwise.
 	 */
-	bool operator==(const Matrix3& other) const;
+	bool operator==(const Matrix4& other) const;
 
 	/**
 	 * Operator that compares this matrix to another matrix to determine if they are not equal.
 	 * @param other The matrix to compare to
 	 * @return True if the two matrices are equal in value. Returns false otherwise.
 	 */
-	bool operator!=(const Matrix3& other) const;
+	bool operator!=(const Matrix4& other) const;
 
 private:
 
 	/**
 	 * The values of this matrix
 	 */
-	float _m[9];
+	float _m[16];
 
 	/**
 	 * Boolean value indicating whether the inverse of this matrix has been computed.
@@ -178,27 +176,35 @@ private:
 	 * Cached inverse of this matrix. Will be null until the inverse is first computed, after which the value may either be
 	 * null or a valid matrix.
 	 */
-	Matrix3* _inverse = nullptr;
+	Matrix4* _inverse = nullptr;
 
 	/**
 	 * Sets the values of this matrix, specified in row-major order
 	 * @param m00 Row 0, column 0 value
 	 * @param m01 Row 0, column 1 value
 	 * @param m02 Row 0, column 2 value
+	 * @param m03 Row 0, column 3 value
 	 * @param m10 Row 1, column 0 value
 	 * @param m11 Row 1, column 1 value
 	 * @param m12 Row 1, column 2 value
+	 * @param m13 Row 1, column 3 value
 	 * @param m20 Row 2, column 0 value
 	 * @param m21 Row 2, column 1 value
 	 * @param m22 Row 2, column 2 value
+	 * @param m23 Row 2, column 3 value
+	 * @param m30 Row 3, column 0 value
+	 * @param m31 Row 3, column 1 value
+	 * @param m32 Row 3, column 2 value
+	 * @param m33 Row 3, column 3 value
 	 */
-	void setValues(float m00, float m01, float m02, float m10, float m11, float m12, float m20, float m21, float m22);
+	void setValues(float m00, float m01, float m02, float m03, float m10, float m11, float m12, float m13,
+		float m20, float m21, float m22, float m23, float m30, float m31, float m32, float m33);
 
 	/**
 	 * Set the values of this matrix from another matrix
 	 * @param other The matrix to copy from
 	 */
-	void setValues(const Matrix3& other);
+	void setValues(const Matrix4& other);
 
 	/**
 	 * Clears the cached inverse of this matrix
@@ -209,5 +215,5 @@ private:
 	 * Sets the cached inverse of this matrix
 	 * @param inv Inverse matrix
 	 */
-	void setInverse(Matrix3* inv);
+	void setInverse(Matrix4* inv);
 };
