4a8d6c40e86a66bb4efdc12147d15983074cb386
naterex <nathanieljrex@gmail.com>
2025-04-09 15:53:27 -0400

    material tests

---
+++ packages/graphics_test/src/materials/BasicMaterialTest.cpp
@@ -0,0 +1,15 @@
+#include <boost/test/unit_test.hpp>
+#include <graphics/materials/BasicMaterial.h>
+#include <common/PrintHelpers.h>
+
+/**
+ * Tests the ability to set the public properties of a BasicMaterial
+ */
+BOOST_AUTO_TEST_CASE(BasicMaterial_properties)
+{
+	BasicMaterialPtr material = BasicMaterial::create();
+	BOOST_TEST(material->type == MaterialType::BASIC);
+
+	material->useVertexColors = true;
+	BOOST_TEST(material->useVertexColors);
+}
