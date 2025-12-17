9badca2ef1acf7af9332628db7b5b005102747de
naterex <nathanieljrex@gmail.com>
2025-12-17 18:30:38 -0500

    Remove ambient lighting from scene

--- packages/graphics_test/src/scene/SceneTest.cpp
+++ packages/graphics_test/src/scene/SceneTest.cpp
@@ -1,6 +1,5 @@
 #include <boost/test/unit_test.hpp>
 #include <graphics/scene/Scene.h>
-#include <graphics/lights/Light.h>
 #include <common/PrintHelpers.h>
 
 /**
@@ -10,8 +9,4 @@ BOOST_AUTO_TEST_CASE(Scene_basics)
 {
 	ScenePtr scene = Scene::create();
 	BOOST_TEST(scene->entityType == EntityType::GROUP);
-
-	LightPtr ambientLighting = scene->ambientLighting;
-	BOOST_TEST(ambientLighting->color == Color::WHITE);
-	BOOST_TEST(ambientLighting->intensity == 0.1f);
 }
