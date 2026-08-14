fae2b0ba8ccea8d216cbc5e5aca04579109b4b87
NateRex <nathanieljrex@gmail.com>
2026-08-13 21:43:20 -0400

    Get directional light working

--- packages/graphics/src/lights/DirectionalLight.cpp
+++ packages/graphics/src/lights/DirectionalLight.cpp
@@ -1,11 +1,26 @@
 #include <graphics/lights/DirectionalLight.h>
 #include <math/Vector3.h>
+#include <math/Matrix3.h>
+#include <common/exceptions/IllegalArgumentException.h>
 #include <common/exceptions/UnsupportedOperationException.h>
 
 DirectionalLight::DirectionalLight(const Vector3& direction): Light(LightType::DIRECTIONAL)
 {
-    // Set forward vector
-    _rotation.setRow(2, direction);
+    if (direction.isZero())
+    {
+        throw IllegalArgumentException("Directional light direction cannot be zero");
+    }
+
+    const Vector3 forward = direction.normalize();
+
+    // Choose a reference axis that is not parallel to forward, then construct
+    // the rest of the orthonormal local-to-world basis.
+    const Vector3 referenceUp = forward.cross(Vector3::YHAT).isZero(0.00001f)
+        ? Vector3::ZHAT
+        : Vector3::YHAT;
+    const Vector3 right = forward.cross(referenceUp).normalize();
+    const Vector3 up = right.cross(forward).normalize();
+    _rotation = Matrix3::fromRows(right, up, forward);
 }
 
 DirectionalLightPtr DirectionalLight::create()
--- packages/graphics_test/src/lights/DirectionalLightTest.cpp
+++ packages/graphics_test/src/lights/DirectionalLightTest.cpp
@@ -1,6 +1,9 @@
 #include <boost/test/unit_test.hpp>
 #include <graphics/lights/DirectionalLight.h>
+#include <math/Matrix3.h>
+#include <common/Utils.h>
 #include <common/PrintHelpers.h>
+#include <common/exceptions/IllegalArgumentException.h>
 #include <common/exceptions/UnsupportedOperationException.h>
 
 /**
@@ -16,6 +19,9 @@ BOOST_AUTO_TEST_CASE(DirectionalLight_basics)
 
     light = DirectionalLight::create(Vector3(0.f, 1.f, 0.f));
     BOOST_TEST(light->getForwardVector() == Vector3(0.f, 1.f, 0.f));
+
+    light->addRotation(Matrix3::fromXRotation(deg2Rad(90.f)));
+    BOOST_TEST(light->getForwardVector().equalTo(Vector3::ZHAT, 0.00001f));
 }
 
 /**
@@ -26,4 +32,5 @@ BOOST_AUTO_TEST_CASE(DirectionalLight_exceptionCases)
 	LightPtr light = DirectionalLight::create();
 	BOOST_REQUIRE_THROW(light->setScaling(1.f, 2.f, 3.f), UnsupportedOperationException);
 	BOOST_REQUIRE_THROW(light->setPosition(1.f, 2.f, 3.f), UnsupportedOperationException);
+	BOOST_REQUIRE_THROW(DirectionalLight::create(Vector3::ZERO), IllegalArgumentException);
 }
