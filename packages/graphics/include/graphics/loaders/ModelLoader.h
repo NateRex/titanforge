fe94fc8ddf7c56a00bdf95d7862b6c40e547a7f2
NateRex <nathanieljrex@gmail.com>
2026-08-14 20:37:57 -0400

    Stashing changes

--- packages/graphics/include/graphics/loaders/ModelLoader.h
+++ packages/graphics/include/graphics/loaders/ModelLoader.h
@@ -9,17 +9,5 @@
  */
 class ModelLoader
 {
-	
-public:
-
-	/**
-	 * Loads a model file while preserving its node hierarchy and local transforms.
-	 * Assimp nodes become EntityGroups, and each mesh referenced by a node becomes
-	 * a child Mesh with a BasicMaterial.
-	 * @param path Path to a model file supported by Assimp
-	 * @return The root entity group of the imported model
-	 * @throws InstantiationException if the model cannot be imported
-	 */
-	static EntityGroupPtr load(const std::string& path);
 
 };
