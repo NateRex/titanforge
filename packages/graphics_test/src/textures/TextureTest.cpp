9433457abf771f6dc60c5c851a5c592fdf9cdce4
NateRex <nathanieljrex@gmail.com>
2026-08-21 15:46:49 -0400

    New Texture tests

--- packages/graphics_test/src/textures/TextureTest.cpp
+++ packages/graphics_test/src/textures/TextureTest.cpp
@@ -1,11 +1,33 @@
 #include <boost/test/unit_test.hpp>
 #include <graphics/textures/Texture.h>
+#include <common/PrintHelpers.h>
 #include <common/exceptions/InstantiationException.h>
+#include <common/exceptions/IllegalArgumentException.h>
+#include <glad/glad.h>
 
 /**
- * Tests the basic constructors and accessors of a texture
+ * Helper method that determines the value for an OpenGL parameter of a texture
+ * @param texture Texture
+ * @param parameter OpenGL parameter
+ * @return Integer value for that parameter on the texture
  */
-BOOST_AUTO_TEST_CASE(Texture_basics)
+int textureParameter(const Texture& texture, unsigned int parameter)
+{
+	int previous = 0;
+	glGetIntegerv(GL_TEXTURE_BINDING_2D, &previous);
+
+	glBindTexture(GL_TEXTURE_2D, texture.id());
+	int value = 0;
+	glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, parameter, &value);
+
+	glBindTexture(GL_TEXTURE_2D, previous);
+	return value;
+}
+
+/**
+ * Tests creation of a texture from an image file
+ */
+BOOST_AUTO_TEST_CASE(Texture_fromImage)
 {
 	TexturePtr texture = Texture::create("assets/container.jpg");
 	BOOST_TEST(texture != nullptr);
@@ -13,9 +35,85 @@ BOOST_AUTO_TEST_CASE(Texture_basics)
 }
 
 /**
- * Tests that an instantiation exception is thrown trying to create a texture using an image that does not exist
+ * Tests that creation of a texture from an image that does not exist results in exception
  */
 BOOST_AUTO_TEST_CASE(Texture_nonExistentImage)
 {
 	BOOST_REQUIRE_THROW(Texture::create("does-not-exist"), InstantiationException);
+}
+
+/**
+ * Tests creation of a texture from a custom storage and sampler config
+ */
+BOOST_AUTO_TEST_CASE(Texture_fromConfig)
+{
+	TextureConfig config;
+	config.width = 8;
+	config.height = 4;
+	config.format = PixelFormat::RGB8;
+	config.sampler.minFilter = TextureFilter::NEAREST;
+	config.sampler.magFilter = TextureFilter::NEAREST;
+	config.sampler.sWrap = TextureWrap::CLAMP_TO_EDGE;
+	config.sampler.tWrap = TextureWrap::MIRRORED_REPEAT;
+	config.generateMipmaps = true;
+
+	TexturePtr texture = Texture::create(config);
+	BOOST_REQUIRE(texture != nullptr);
+	BOOST_TEST(texture->id() != 0);
+	BOOST_TEST(texture->width() == config.width);
+	BOOST_TEST(texture->height() == config.height);
+	BOOST_TEST(texture->format() == config.format);
+	BOOST_TEST(texture->config().sampler.minFilter == config.sampler.minFilter);
+	BOOST_TEST(texture->config().sampler.magFilter == config.sampler.magFilter);
+	BOOST_TEST(texture->config().sampler.sWrap == config.sampler.sWrap);
+	BOOST_TEST(texture->config().sampler.tWrap == config.sampler.tWrap);
+	BOOST_TEST(texture->config().generateMipmaps == config.generateMipmaps);
+	BOOST_TEST(textureParameter(*texture, GL_TEXTURE_WIDTH) == 8);
+	BOOST_TEST(textureParameter(*texture, GL_TEXTURE_HEIGHT) == 4);
+	BOOST_TEST(textureParameter(*texture, GL_TEXTURE_INTERNAL_FORMAT) == GL_RGB8);
+}
+
+/**
+ * Tests resizing texture storage
+ */
+BOOST_AUTO_TEST_CASE(Texture_resize)
+{
+	TextureConfig config;
+	config.width = 2;
+	config.height = 3;
+	config.format = PixelFormat::RGBA8;
+	config.sampler.sWrap = TextureWrap::CLAMP_TO_BORDER;
+
+	TexturePtr texture = Texture::create(config);
+
+	texture->resize(7, 5);
+	BOOST_TEST(texture->width() == 7);
+	BOOST_TEST(texture->height() == 5);
+	BOOST_TEST(texture->format() == config.format);
+	BOOST_TEST(texture->config().sampler.sWrap == config.sampler.sWrap);
+	BOOST_TEST(textureParameter(*texture, GL_TEXTURE_WIDTH) == 7);
+	BOOST_TEST(textureParameter(*texture, GL_TEXTURE_HEIGHT) == 5);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+/**
+ * Tests validation of texture dimensions during creation and resize
+ */
+BOOST_AUTO_TEST_CASE(Texture_invalidDimensions)
+{
+	TextureConfig config;
+	config.width = 0;
+	BOOST_CHECK_THROW(Texture::create(config), IllegalArgumentException);
+
+	config.width = 1;
+	config.height = 0;
+	BOOST_CHECK_THROW(Texture::create(config), IllegalArgumentException);
+
+	config.height = 1;
+	TexturePtr texture = Texture::create(config);
+	BOOST_CHECK_THROW(texture->resize(0, 1), IllegalArgumentException);
+	BOOST_CHECK_THROW(texture->resize(1, 0), IllegalArgumentException);
+	BOOST_TEST(texture->width() == 1);
+	BOOST_TEST(texture->height() == 1);
 }
