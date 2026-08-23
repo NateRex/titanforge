5968cc75c96aad48c8b4e53f1836e914502eb1b2
NateRex <nathanieljrex@gmail.com>
2026-08-23 14:58:43 -0400

    Texture refactor 1

--- packages/graphics/include/graphics/textures/pointers/CubeTexturePtr.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <memory>
-
-/**
- * Shared pointer to a CubeTexture instance
- */
-using CubeTexturePtr = std::shared_ptr<class CubeTexture>;
---
+++ packages/graphics/include/graphics/textures/pointers/TextureCubePtr.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <memory>
+
+/**
+ * Shared pointer to a TextureCube instance
+ */
+using TextureCubePtr = std::shared_ptr<class TextureCube>;
